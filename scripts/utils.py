d3b7adeb492fb6fe9c64f3b5cc7543d2f9aa7f19
Rishik00 <mrishikesh104@gmail.com>
2025-12-24 20:24:29 +0530

    removed secrets

--- scripts/utils.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import login
-
-# Hugging Face authentication
-HF_TOKEN_RISHIK66 = "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"
-
-def login_to_hf():
-    """Login to Hugging Face Hub."""
-    login(token=HF_TOKEN_RISHIK66)
-    print("[INFO] Logged in to Hugging Face successfully!")
